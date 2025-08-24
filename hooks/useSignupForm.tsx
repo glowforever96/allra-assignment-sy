64e5c59e2b66c5451dd6b93d9beef5af2587c3c1
boyfromthewell <tnsdyd10@naver.com>
2025-08-24 16:55:16 +0900

    feat: 로그인 페이지 아이디 저장 체크박스 활성화시 사업자 번호 localStoarge 저장 커스텀 훅

--- hooks/useSignupForm.tsx
+++ hooks/useSignupForm.tsx
@@ -75,18 +75,6 @@ function useSignupForm() {
     }
   };
 
-  // 숫자만 입력 가능, 최대 길이 제한 함수
-  const handleNumberOnlyInput = (
-    e: React.FormEvent<HTMLInputElement>,
-    maxLength: number
-  ) => {
-    const target = e.target as HTMLInputElement;
-    target.value = target.value.replace(/\D/g, "");
-    if (target.value.length > maxLength) {
-      target.value = target.value.slice(0, maxLength);
-    }
-  };
-
   useEffect(() => {
     updateStep("businessNumber", isBusinessNumVerified);
   }, [isBusinessNumVerified, updateStep]);
@@ -136,7 +124,6 @@ function useSignupForm() {
     isBusinessNumVerified,
     handleBusinessNumVerify,
     handleBlur,
-    handleNumberOnlyInput,
     onSubmitSignup,
   };
 }
