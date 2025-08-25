2cf7b69f0d7c0fb6f2acb215b7112eea0c41c4cc
boyfromthewell <tnsdyd10@naver.com>
2025-08-26 00:49:37 +0900

    feat: 블로그 목록 조회 api 요청 코드 작성, 응답 값 타입 정의

--- api/blogs.ts
+++ api/blogs.ts
@@ -1,11 +1,36 @@
-import { BlogsBannerData } from "@/types";
+import { BlogResponse, BlogsBannerData } from "@/types";
+
+const revalidate = 3600; // 정적 데이터는 1시간 마다 재검증
 
 export async function getBlogsBanner(): Promise<BlogsBannerData[]> {
   const res = await fetch(
     "https://allra-front-assignment.vercel.app/api/blogs/banners",
     {
-      cache: "force-cache", // 정적 배너 데이터이기 때문에 1시간 마다 재검증
-      next: { revalidate: 3600 },
+      cache: "force-cache",
+      next: { revalidate },
+    }
+  );
+  return res.json();
+}
+
+export async function getBlogs({
+  page,
+  term,
+}: {
+  page: number;
+  term?: string;
+}): Promise<BlogResponse> {
+  const searchParams = new URLSearchParams();
+
+  if (page) searchParams.set("page", page.toString());
+  if (term) searchParams.set("term", term);
+
+  const res = await fetch(
+    `https://allra-front-assignment.vercel.app/api/blogs?pageSize=12&${searchParams.toString()}`,
+    {
+      // 검색어가 있다면 캐시 전략 사용하지 않음, 요청시마다 최신 결과 반영
+      cache: term ? "no-store" : "force-cache",
+      next: term ? undefined : { revalidate },
     }
   );
   return res.json();
--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,27 @@
-export interface BlogsBannerData {
+interface BlogsBannerData {
   id: number;
   title: string;
   thumbnail: string;
   summary: string;
 }
+
+type Category = "TREND" | "TIP" | "NEWS" | "GUIDE" | "EXPERIENCE";
+interface Blog {
+  id: number;
+  title: string;
+  category: Category;
+  thumbnail: string;
+  showCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface BlogResponse {
+  list: Blog[];
+  totalCount: number;
+  totalPages: number;
+  page: number;
+  pageSize: number;
+}
+
+export type { BlogsBannerData, Blog, BlogResponse };
