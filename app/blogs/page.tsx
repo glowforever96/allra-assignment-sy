36825cb46098de6d776d9dd169bd516596a68d09
boyfromthewell <tnsdyd10@naver.com>
2025-08-26 01:54:09 +0900

    feat: blogs 페이지 검색 input 컴포넌트 구현, 기능 구현

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import { getBlogs, getBlogsBanner } from "@/api/blogs";
 import TopBanner from "@/components/top-banner";
 import BlogList from "@/components/blog-list";
+import SearchInput from "@/components/search-input";
 
 export default async function BlogPage({
   searchParams,
@@ -19,14 +20,17 @@ export default async function BlogPage({
   return (
     <section className="flex flex-col pb-13">
       <div className="flex flex-col">
-        <h2
-          className="text-title-3 font-bold text-label-800 md:text-title-2 lg:text-title-1 tracking-title-3 leading-title-3
+        <div className="flex flex-col gap-6 md:flex-row md:justify-between">
+          <h2
+            className="text-title-3 font-bold text-label-800 md:text-title-2 lg:text-title-1 tracking-title-3 leading-title-3
           md:tracking-title-2 lg:tracking-title-1 md:leading-title-2 lg:leading-title-1"
-        >
-          블로그
-        </h2>
-        <TopBanner bannerData={bannerData} />
-        <BlogList blogData={blogData} />
+          >
+            블로그
+          </h2>
+          <SearchInput />
+        </div>
+        {!term && <TopBanner bannerData={bannerData} />}
+        <BlogList blogData={blogData} searchValue={term} />
       </div>
     </section>
   );
